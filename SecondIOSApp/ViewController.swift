d850839d8f7f8262f4ececadd931f0a6dc282852
Bri Hewlett <bhewlett00@gmail.com>
2019-10-24 10:14:49 -0400

    Added Monster logic.

--- SecondIOSApp/ViewController.swift
+++ SecondIOSApp/ViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 //equivalent of the MainActivity Java class in Android
 class ViewController: UIViewController {
 
+    //Gives ViewController access to the ImageView
+    @IBOutlet weak var monsterImageView: UIImageView!
+    
+    //created an array of Strings that contains the names of the different monsters
+    var monsters = ["Astro", "Fluffy", "Munchie", "Squido"]
+    
     //equivalent of the onCreate mehtod in Java Android classes
     //it's called when the sceen that it controls is accessed
     //initialization work is done in this method
@@ -27,6 +33,16 @@ class ViewController: UIViewController {
         }
     }
 
-
+    //Gives ViewController the ability to respond to the selection of segments in the segmented control
+    @IBAction func changeMonster(_ sender: UISegmentedControl) {
+        //chnage the image in the ImageView based on the segment selected
+        
+        //get the index of the selected segment (0, 1, 2, 3)
+        let index = sender.selectedSegmentIndex
+        
+        //set the image of the ImageView to a new UIImage
+        //the new UIImage is the elements of the monsteres array that matches the index
+        monsterImageView.image = UIImage(named: monsters[index])
+    }
 }
 
